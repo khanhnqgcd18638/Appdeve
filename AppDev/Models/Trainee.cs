976dac3b66431394acc097ffcfe806c1f527e564
OS <OS@DESKTOP-NLC0EVM>
2021-02-04 18:59:37 +0700

    Add project files.

---
+++ AppDev/Models/Trainee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDev.Models
+{
+    public class Trainee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string DateofBirth { get; set; }
+        public string Education { get; set; }
+        public string MainProgrammingLang { get; set; }
+        public float ToeicScore { get; set; }
+        public string Exp_Detail { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ AppDev/Models/Trainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDev.Models
+{
+    public class Trainer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Education { get; set; }
+        public string WorkPlace { get; set; }
+        public string Telephone { get; set; }
+        public string Type { get; set; }
+    }
+
+}
